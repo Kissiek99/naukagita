3691d3f457c68e4a8cb9a6a20ba5da67954beb94
Kissiek99 <kwojdyna99@gmail.com>
2024-04-06 11:14:29 +0200

    zadanie 1

---
+++ Main.java
@@ -0,0 +1,13 @@
+package naukagita;
+
+public class Main {
+    public static void main(String[] args) {
+        // Tworzenie obiektu klasy Point i inicjowanie jego wartości
+        Point point = new Point(3.5, 2.0);
+
+        // Wyświetlanie współrzędnych punktu
+        System.out.println("Współrzędne punktu:");
+        System.out.println("x = " + point.getX());
+        System.out.println("y = " + point.getY());
+    }
+}
